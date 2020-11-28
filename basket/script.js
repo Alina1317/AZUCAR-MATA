43d4acd6a6fed24ba578e84c4f55f25acb5b165f
Дорошенко Алина <belyanina.07@mail.ru>
2020-11-29 00:15:58 +0300

    refactor: change hover link

--- basket/script.js
+++
@@ -1,9 +0,0 @@
-const burg = document.querySelector('.header-burger'),
-	burgLine = document.querySelector('.header-menu'),
-	header = document.querySelector('.header');
-
-burg.onclick = () => {
-	burg.classList.toggle('toggle');
-	burgLine.classList.toggle('toggle');
-	header.classList.toggle('toggle');
-}
