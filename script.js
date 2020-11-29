9f0ee7a4e77595947aaeb0f1771647bb63e035c8
Дорошенко Алина <belyanina.07@mail.ru>
2020-11-29 16:51:40 +0300

    fix: change footer index basket and product

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const burg = document.querySelector('.header-burger'),
 	burgLine = document.querySelector('.header-menu'),
 	header = document.querySelector('.header');
 
+const buyBtn = document.getElementById('buy'),
+	basketBtn = document.getElementById('basket');
+
 burg.onclick = () => {
 	burg.classList.toggle('toggle');
 	burgLine.classList.toggle('toggle');
 	header.classList.toggle('toggle');
 }
+
+buyBtn.addEventListener = () => {
+
+};
+
+basketBtn.addEventListener = () => {
+
+};
