20a58513e6648f33fd5aa4582e5dabecdc4f9577
Дорошенко Алина <belyanina.07@mail.ru>
2020-12-07 15:24:23 +0300

    feat: add new img im home block, and text in block about

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const burg = document.querySelector('.header-burger'),
 	burgMenu = document.querySelector('.header-menu'),
 	header = document.querySelector('.header');
 
-const buyBtn = document.querySelector('.coffe-shop__bye'),
-	basketBtn = document.querySelector('.coffe-shop__add-basket');
-
+//header
 burg.onclick = () => {
 	burg.classList.toggle('toggle');
 	burgMenu.classList.toggle('toggle');
 	header.classList.toggle('toggle');
-}
+};
 
-// buyBtn.onclick = e => {
-// 	console.log(e);
-// 	console.log(e.target);
-// }
+const btnBuy = document.querySelector('.coffe-shop__bye'),
+ btnBasket = document.querySelector('.coffe-shop__add-basket'),
+	total = document.querySelector('.total');
+let count = 0;
 
-//buyBtn.addEventListener('click', );
-
-//basketBtn.addEventListener('click',);
+btnBuy.addEventListener('click', (e) => {
+	//console.log(e.target);
+	if(e.target) {
+		count++;
+		btnBasket.classList.add('top');
+		total.className = 'num';
+		total.textContent = count;
+	}
+	
+});
