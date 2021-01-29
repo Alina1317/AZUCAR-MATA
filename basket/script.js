e3472f78315cc1f7ec7e1476a0e540c03d6d5235
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-01-29 20:46:47 +0300

    feat: hz

--- basket/script.js
+++ basket/script.js
@@ -9,6 +9,7 @@ const total = document.getElementById('total');
 let count = 1;
 let storage = {};
 let totalPrice = null;
+let finalTotalPrice;
 // let deliveryPrice = {};
 // счетчик в корзинку
 let countTotal = 0 || +localStorage.getItem(`count-total`);
@@ -51,8 +52,8 @@ function innerDeliveryPrice (json_price) {
 	europe.textContent = json_price.europe + "€"
 
 	function clickOnDelivery (price) {
-		finalTotalPrice = totalPrice + price
-		setTotalPrice(finalTotalPrice)
+		finalTotalPrice = totalPrice + price;
+		setTotalPrice(finalTotalPrice);
 		document.querySelectorAll(".wrapper-shop__total-delete").forEach(el => el.removeEventListener("click", deleteCard))
 		document.querySelectorAll(".wrapper-shop__product-count").forEach(el => el.removeEventListener("click", changeQuant))
 		const CloseModal = () => {
@@ -67,17 +68,37 @@ function innerDeliveryPrice (json_price) {
 		document.querySelectorAll(".modal__close").forEach(el => el.addEventListener("click", CloseModal))
 	}
 
+	function paypalButtonCreate(elem) {
+		let tmpElem = document.createElement('div');
+		tmpElem.innerHTML = `
+		<div class="paypal">
+		<div id="smart-button-container">
+		<div style="text-align: center;">
+		<div id="paypal-button-container"></div>
+		</div>
+		</div>
+		<script src="https://www.paypal.com/sdk/js?client-id=AbKJL7fs4sOVe0yBAQwBFFoDNv9qWkZZzeT1NkMRz9jP4978cWT0TBdNXRpVnyQ-p0WcsW2o_Mu_QsfJ&currency=EUR" data-sdk-integration-source="button-factory"></script>
+		</div>`;
+
+		elem.appendChild(tmpElem);
+		initPayPalButton();
+	}
+
 	spain.parentElement.onclick = (e) => {
+		paypalButtonCreate(spain)
 		clickOnDelivery(json_price.spain)
 		setTotalPrice(finalTotalPrice)
 	}
 	europe.parentElement.onclick = (e) => {
+		paypalButtonCreate(europe)
 		clickOnDelivery(json_price.europe)
-		setTotalPrice(finalTotalPrice)
+		setTotalPrice(finalTotalPrice);
+
 	}
 	free.parentElement.onclick = (e) =>{
-		clickOnDelivery(0)
-		setTotalPrice(totalPrice)
+		paypalButtonCreate(free);
+		clickOnDelivery(0);
+		setTotalPrice(totalPrice);
 	}
 }
 
@@ -303,6 +324,7 @@ fetch('../db.json')
 // PayPal
 
 function initPayPalButton() {
+
 	paypal.Buttons({
 		style: {
 			shape: 'rect',
@@ -312,27 +334,56 @@ function initPayPalButton() {
 		},
 
 		createOrder: function (data, actions) {
+
 			console.log(storage);
+
+			const result = [];
+			// Получаю массив ключей storage
+			for (let key in storage) {
+				if (storage.hasOwnProperty(key)) {
+					result.push(key)
+				}
+			}
+			// Делаю из storage массив с объектами
+			let newStore = [];
+			result.forEach(elem => {
+				newStore.push(storage[elem])
+			});
+
+
+
+			// console.log(Object.keys(storage["coffe-id-1"])); // массив ключей вложенного объекта
+
+
+
+
+			// const priceWithDelivery = document.querySelector('.wrapper-price__total-price').innerHTML;
+			const delivery = {
+				area: 'Delivery in Europe',
+				price: (finalTotalPrice - totalPrice).toFixed(2)
+			};
+
+			// function createItems() {
+			// 	Object.keys(storage).forEach(key => {
+			// 		let value = storage[key];
+			//
+			// 		console.log(`${key}: ${value}`);
+			// 	});
+			// }
+			//
+			// createItems();
+
 			return actions.order.create({
 				purchase_units: [
 					{
 						amount: {
-							currency_code: "EUR", value: totalPrice + 300,
+							currency_code: "EUR",
+							value: finalTotalPrice,
 							breakdown: {
-								item_total: {currency_code: "EUR", value: totalPrice + 300,}
+								item_total: {currency_code: "EUR", value: finalTotalPrice}
 							}
 						},
 						items: [
-							// Образец для создания аетйма
-							{
-								name: "Coffee 2",
-								// description: "Необязательное поле, можно сюда добавлять например тип упаковки",
-								unit_amount: {
-									currency_code: "EUR",
-									value: "100.00"
-								},
-								quantity: "3"
-							},
 							// Код для одной из позиций (нужно додумать как подставлять ключи из storage динамически)
 							{
 								name: storage["coffe-id-1"].name,
@@ -342,6 +393,14 @@ function initPayPalButton() {
 								},
 								quantity: storage["coffe-id-1"].count
 							},
+							{
+								name: delivery.area,
+								unit_amount: {
+									currency_code: "EUR",
+									value: delivery.price
+								},
+								quantity: 1
+							}
 						],
 					}
 				]
@@ -360,5 +419,3 @@ function initPayPalButton() {
 
 	}).render('#paypal-button-container');
 }
-
-initPayPalButton();
